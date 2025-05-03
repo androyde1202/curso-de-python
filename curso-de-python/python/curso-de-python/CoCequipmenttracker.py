14b1143287c69f8982fdddd092b2974c777676c4
androyde1202 <andreypereira1202@gmail.com>
2025-05-03 10:25:31 -0300

    CoCequipmenttracker.py

--- curso-de-python/python/curso-de-python/CoCequipmenttracker.py
+++ curso-de-python/python/curso-de-python/CoCequipmenttracker.py
@@ -1,5 +1,7 @@
-equipamento=str(input("qual equipamento você dejesa melhorar:"))
+equipamento=str(input("qual equipamento você dejesa melhorar coloque com todas asprimeiras çetras maiusculas e em ingles:"))
 raridade=str("N/E")
 if equipamento == "Giant Gauntlet" or "Spiky Ball" or "Snake Bracelet" or "Frozen Arrow" or "Magic Mirror" or "Action Figure" or "Fireball" or "Lavaloon Puppet" or "Rocket Spear" or "Electro Boots":
     raridade=str("epica")
-elif equipamento == "Barbarian Puppet" "Rage Vial" "Earthquake Boots" "Vampstache" "Archer Puppet" "Invisibility Vial" "Giant Arrow" "Healer Puppet" "Dark Orb" "Henchmen Puppet" "Metal Pants" "Noble Iron" "Eternal Tome" "Life Gem" "Rage Gem" "Healing Tome" "" "" "" ""
+else equipamento == "Barbarian Puppet" "Rage Vial" "Earthquake Boots" "Vampstache" "Archer Puppet" "Invisibility Vial" "Giant Arrow" "Healer Puppet" "Dark Orb" "Henchmen Puppet" "Metal Pants" "Noble Iron" "Eternal Tome" "Life Gem" "Rage Gem" "Healing Tome" "Royal Gem" "Seeking Shield" "Hog Rider Puppet" "Haste Vial":
+    raridade=str("comum")
+if
